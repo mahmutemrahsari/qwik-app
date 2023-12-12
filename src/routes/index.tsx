b2bd898d92c39a191dce30858372dd3507f603c3
Mahmut Emrah Sari <mahmutemrahsari@gmail.com>
2023-12-12 15:14:38 +0100

    initial test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,52 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useStore } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const count = useSignal(0);
+  const state = useStore({ count: 0 });
+  const store = useStore(
+    {
+      nested: {
+        fields: { are: "also tracked" },
+      },
+      list: ["item 1"],
+    }
+    // { deep: false } //only track the top-level properties.
+  );
+
+  console.log(count.value);
   return (
     <>
       <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
+
+      <button onClick$={() => count.value++}>
+        {" "}
+        INCREMENT by using useSignal {count.value}
+      </button>
+      <button onClick$={() => state.count++}>
+        {" "}
+        INCREMENT by using useStore {state.count}
+      </button>
+
+      <div>
+        <p>{store.nested.fields.are}</p>
+      </div>
+
+      <div>
+        <button
+          onClick$={() => {
+            store.list.push(`Item ${store.list.length}`);
+          }}
+        >
+          Add to list
+        </button>
+        <ul>
+          {store.list.map((item, index) => (
+            <li key={`items-${index}`}>{item}</li>
+          ))}
+        </ul>
+        <p>List Length: {store.list.length}</p>
+      </div>
     </>
   );
 });
